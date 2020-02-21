13d9bc1664f24e60324629fc1e88ba28137cc3a4
root <root@DESKTOP-HH0OESJ.localdomain>
2020-02-21 10:52:35 +0530

    builder pattern

---
+++ builder-pattern/builderpatterndemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace builderpatterndemo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+            Console.ReadLine();
+        }
+    }
+}
